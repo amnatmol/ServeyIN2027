3dd95bd7cebeb9df29f94bba6b0dc0b59f63f11e
amnatmol <volunamnat66@gmail.com>
2026-08-26 11:36:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-  './',
   './index.html',
   './manifest.json',
   './icon-192.png'
@@ -8,12 +7,21 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
